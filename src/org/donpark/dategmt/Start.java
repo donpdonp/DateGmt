475e823f06ebb8e678412f89a1285c91bab87406
Don Park <don@donpark.org>
2010-07-03 15:38:59 -0700

    a bit more verbose, and focus on getTimeZone

--- src/org/donpark/dategmt/Start.java
+++ src/org/donpark/dategmt/Start.java
@@ -7,6 +7,7 @@ import java.util.TimeZone;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Start extends Activity {
@@ -19,10 +20,18 @@ public class Start extends Activity {
 		DateFormat datePattern;
 		long offset = 1275691189000L;
 		datePattern =  new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ssZ");
-		datePattern.setTimeZone(TimeZone.getTimeZone("GMT"));
+		String zonename = "GMT";
+		TimeZone timezone = TimeZone.getTimeZone(zonename);
+		datePattern.setTimeZone(timezone);
 		String date_str = datePattern.format(new Date(offset));
 		
-        TextView t = (TextView) findViewById(R.id.t);
+        TextView t = (TextView) findViewById(R.id.t1);
+        t.setText(zonename + " " + timezone);   
+        Log.i("DateGmt", zonename + " " + timezone);
+
+        t = (TextView) findViewById(R.id.t2);
         t.setText(date_str);
+        Log.i("DateGmt", date_str);
+
     }
 }
